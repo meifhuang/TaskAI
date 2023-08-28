c0b698b195fd9ebe642d301e6a7be48567c95689
Mei Huang <100555326+meifhuang@users.noreply.github.com>
2023-08-28 18:58:24 -0400

    Implement delete and edit tasks functionality

--- to-do/src/components/SingleTodo.tsx
+++ to-do/src/components/SingleTodo.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Todo} from "../model"; 
 import '../styles.css'; 
 
@@ -10,28 +10,43 @@ interface Props {
 
 const SingleToDo: React.FC<Props> = ({todo, todos, setTodos}) => {
 
+    const [edit, setEdit] = useState<boolean>(false); 
+    const [editTodo, setEditTodo] = useState<string>(todo.todo); 
+
     const handleDone = (id: number) => {
         setTodos(
             todos.map((todo) => 
-            todo.id === id ? {...todo, isDone: todo.isDone} : todo ))
+            todo.id === id ? {...todo, isDone: !todo.isDone} : todo ))
     }
+
     const handleDelete = (id: number) => {
         setTodos(
             todos.filter((todo) => 
             todo.id !== id
             ))
         }
+
+    const handleEdit = (e:React.FormEvent, id: number) => {
+      e.preventDefault();
+      setTodos(todos.map((todo) => (
+        todo.id === id ? {...todo, todo: editTodo} : todo
+      )))
+      setEdit(false)
+    }
     
       return (
-      <form className="todo_item"> 
+      <form className="todo_item" onSubmit={(e)=>handleEdit(e,todo.id)}> 
         <div className="todo_text"> 
-            {!todo.isDone ? 
-              <span> {todo.todo} </span> :
-              <span> completed </span>
+            {edit ? (<input value={editTodo} onChange={(e)=> setEditTodo(e.target.value)} />) : todo.isDone ? 
+              (<s> {todo.todo} </s>) :
+              (<span> {todo.todo} </span>)
             }
-            <button className='todo_action'>Edit</button>
-            <button className='todo_action' onClick={()=>handleDelete}>Delete</button>
-            <button className='todo_action' onClick={()=>handleDone(todo.id)} >Mark Done</button>
+            <button type='button' className='todo_action' onClick={()=> {
+              if (!edit && !todo.isDone) {
+              setEdit(!edit)
+            }}} > Edit </button>
+            <button type='button' className='todo_action' onClick={()=>handleDelete(todo.id)}>Delete</button>
+            <button type='button' className='todo_action' onClick={()=>handleDone(todo.id)}>Mark</button>
         </div>
       </form>
       )
