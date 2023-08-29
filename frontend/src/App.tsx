6888a084c9ad2aebcdf3f52e3bbcd70181c0d0ee
Mei Huang <100555326+meifhuang@users.noreply.github.com>
2023-08-29 19:07:01 -0400

    Create voice command to add something to to-do list

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,30 +2,24 @@ import React, {useState} from 'react';
 import './App.css';
 import './styles.css'; 
 import InputField from "./components/InputField"
-import ToDoList from "./components/ToDoList"
 import VirtualAssistant from "./components/VirtualAssistant"
 import {Todo} from "./model"; 
 
 
-
 const App: React.FC = () => {
-  const [todo, setTodo] = useState<string> ("");
-  const [todos, setTodos] = useState<Todo[]>([]); 
-
-
-  const handleAdd = (e: React.FormEvent) => {
-    e.preventDefault();
-    if (todo) {
-      setTodos([...todos, {id: Date.now(), todo: todo, isDone: false}])
-      setTodo("")
-    }
-  };
+ 
+  // const handleAdd = (e: React.FormEvent) => {
+  //   e.preventDefault();
+  //   if (todo) {
+  //     setTodos([...todos, {id: Date.now(), todo: todo, isDone: false}])
+  //     setTodo("")
+  //   }
+  // };
   
   return (
     <div className="App">
       <h1 className="heading"> TaskAI </h1>
       {/* <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd}/> */}
-      <ToDoList todos={todos} setTodos={setTodos}/>
       <VirtualAssistant/>
     </div>
   );
--- frontend/src/components/VirtualAssistant.tsx
+++ frontend/src/components/VirtualAssistant.tsx
@@ -1,31 +1,103 @@
 import React, {useState} from 'react';
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
+import ToDoList from "./ToDoList"
+import {Todo} from "../model"; 
 
 
 const VirtualAssistant: React.FC= () => {
+
+  const commands = [
+    {
+      command: 'reset',
+      callback: () => {
+        resetTranscript()
+        setMessage('you may restart your command')
+      } 
+    },
+    {
+      command: ['hello', 'hi', 'hey'],
+      callback: () => setMessage('hello there')
+    },
+    {
+      command: ['add * to my list'], 
+      callback: (command:string) => {
+        console.log(command)
+        setTodo(command)
+        setTodos((prev) => [...prev, {id: Date.now(), todo: command, isDone: false}])
+        setTodo("")
+        resetTranscript()
+        setMessage('adding...')
+        setTimeout(()=>setMessage('added'),2000)
+      }
+    },
+    {
+      command: 'stop listening',
+      callback: () => handleStopListening()
+    },
+    {
+      command: '*weather*',
+      callback: () => {
+           setMessage('the weather outside is frightful')
+        }, 
+    },
+    { command: ['completed * task'],
+      callback: (item:string) => {
+        setMessage(`you selected task: ${item} Is that correct?`)
+      } 
+    }
+  ]
+
+  const [message, setMessage] = useState<string>(""); 
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]); 
 
   const {
     transcript,
     listening,
     resetTranscript,
     browserSupportsSpeechRecognition
-  } = useSpeechRecognition();
+  } = useSpeechRecognition({commands});
+
 
   if (!browserSupportsSpeechRecognition) {
     return <span>Browser doesn't support speech recognition.</span>;
   }
 
+  // const handleAdd = (task:string) => {
+  //     console.log('called')
+  //     let extraction = transcript
+  //     console.log(extraction)
+  //     let add_index = extraction.indexOf('list')
+  //     let task = extraction.substring(add_index+4)
+  //     console.log(task)
+  //     if (task) {
+  //       setTodo(task)
+  //       setTodos((prev) => [...prev, {id: Date.now(), todo: task, isDone: false}])
+  //       console.log(task)
+  //       setMessage(`adding ${task}`)
+  //     }
+  // }
+
   const handleStartListening = () => {
-    SpeechRecognition.startListening();
+    console.log('listening')
+    SpeechRecognition.startListening({continuous:true});
   };
+
+  const handleStopListening = () => {
+    console.log('stop')
+    SpeechRecognition.stopListening();
+    resetTranscript()
+  }
 
   return (
     <div> 
+      <ToDoList todos={todos} setTodos={setTodos}/>
       <p> Microphone: {listening ? 'on' : 'off' } </p>
       <button onClick={handleStartListening}>Start</button>
-      <button onClick={SpeechRecognition.stopListening}>Stop</button>
-      <button onClick={resetTranscript}>Reset</button>
-      <p>{transcript}</p>
+      <button onClick={handleStopListening}>Stop</button>
+      {/* <button onClick={resetTranscript}>Reset</button> */}
+      <p> {transcript} </p>
+      <p>{message}</p>
     </div>
   )
   }
@@ -37,5 +109,6 @@ const VirtualAssistant: React.FC= () => {
     //     onChange={(e)=> setMessage(e.target.value)}/>  
     //     <button className='input_submit' type='submit' > Enter instructions </button>
     // </form> 
+    
 export default VirtualAssistant;
 
