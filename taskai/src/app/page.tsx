9854a10d80f0ed69faf8fb4afe0c92749d186476
Mei Huang <100555326+meifhuang@users.noreply.github.com>
2023-09-05 17:10:24 -0400

    Implement add

--- taskai/src/app/page.tsx
+++ taskai/src/app/page.tsx
@@ -1,8 +1,19 @@
 import Link from "next/link"
+import {ToDoItem} from "./components/ToDoItem"
 import {prisma} from '@/db'
+import {redirect} from "next/navigation"
+
+async function createTodo(data: FormData) {
+  "use server"
+  const title = data.get("title")?.valueOf()
+  if (typeof title !== "string" || title.length === 0) {
+    throw new Error("Invalid Title")
+  }
+  await prisma.todo.create({data: {title: title, complete: false}})
+  redirect("/")
+}
 
 export default async function Home() {
-
   
   const date = new Date();
   const dow = date.toLocaleString('default', {weekday: 'long'})
@@ -30,9 +41,23 @@ export default async function Home() {
     </div>
     <ul className="pl-4"> 
     { todos.map(todo => (
-      <li key={todo.id}> - {todo.title} </li>
+      <ToDoItem title={todo.title} id={todo.id} complete={todo.complete} />
     ))}
+
     </ul>
+    <form action={createTodo} className="flex gap-2 flex-col">
+      <input
+        type="text"
+        name="title"
+        className="border border-slate-500"
+        />
+      <button 
+        type="submit"
+        className="border"
+      > 
+        Submit
+      </button>
+    </form>
     {/* <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd}/> */}
   
   </>
