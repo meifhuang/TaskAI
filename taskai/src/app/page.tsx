3ba71ccf8e82f5e01f1f74b5938430ea67ee14e4
Mei Huang <100555326+meifhuang@users.noreply.github.com>
2023-09-04 23:55:03 -0400

    Refactor todo app

--- taskai/src/app/page.tsx
+++ taskai/src/app/page.tsx
@@ -1,3 +1,41 @@
-export default function Home() {
-  return <h1> Hello</h1>
+import Link from "next/link"
+import {prisma} from '@/db'
+
+export default async function Home() {
+
+  
+  const date = new Date();
+  const dow = date.toLocaleString('default', {weekday: 'long'})
+  const month = date.toLocaleString('default', {month: 'short'})
+  const day = date.getDate()
+  const year = date.getFullYear()
+
+  const todos = await prisma.todo.findMany()
+  // await prisma.todo.create({data: {title: "test", complete: false}})
+
+  return (
+    <> 
+    <header className="flex items-center mb-4"> 
+      <h1 className="text-5xl"> TaskAI </h1> 
+      
+    </header>
+
+    <div className="flex items-center mb-3">
+    {/* <div className='flex'> */}
+        <h3 className="text-2xl"> {dow}, {month} {day} </h3>
+      {/* </div> */}
+      {/* <div className="add_task" onClick={(e)=> handleAdd(e)}>
+          <AddIcon className="add_icon"/> Add Task
+      </div> */}
+    </div>
+    <ul className="pl-4"> 
+    { todos.map(todo => (
+      <li key={todo.id}> - {todo.title} </li>
+    ))}
+    </ul>
+    {/* <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd}/> */}
+  
+  </>
+    )
+
 }
--- taskai/src/db.ts
+++ taskai/src/db.ts
@@ -1,17 +1,12 @@
 import { PrismaClient } from '@prisma/client'
 
-const prismaClientSingleton = () => {
-  return new PrismaClient()
-}
-
-type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>
-
 const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClientSingleton | undefined
+  prisma: PrismaClient | undefined
 }
 
-const prisma = globalForPrisma.prisma ?? prismaClientSingleton()
-
-export default prisma
+export const prisma = globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ['query'],
+  })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
