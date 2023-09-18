9bc9d2a3df8aaf75350c10c5514802dfb482764a
Mei Huang <100555326+meifhuang@users.noreply.github.com>
2023-09-17 20:43:33 -0400

    Create login function

--- server/src/controller/auth.ts
+++ server/src/controller/auth.ts
@@ -2,6 +2,11 @@ import {Request, Response} from 'express';
 import * as jwtUtils from '../utils/jwt';
 import User from "../models/User"
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken'; 
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+const JWT_SECRET = process.env.JWT_SECRET || 's3cr3T'
 
 
 export async function register(req: Request, res: Response): Promise<void> {
@@ -16,7 +21,7 @@ export async function register(req: Request, res: Response): Promise<void> {
         const hashedPass = await bcrypt.hash(password, 10);
         const user = await User.create({firstname, username, password: hashedPass, email})
         res.status(201).json(user) 
-        }
+    }
     catch (err: any) {
         console.error(err); 
         if (err.name === 'SequelizeValidationError') {
@@ -28,5 +33,29 @@ export async function register(req: Request, res: Response): Promise<void> {
         else {
             res.status(500).json({message: 'Internal server error'})
         }
+    }
+}
+
+export async function login(req: Request, res: Response): Promise<void> {
+    const {username, password} = req.body
+
+    try {
+        const user = await User.findOne({where: {username}})
+        if (!user) {
+            res.status(401).json({message: 'Invalid credentials'})
+            return;
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password) 
+        if (!isPasswordValid) {
+            res.status(401).json({message: 'Incorrect password'})
+        } 
+        
+        const token = jwtUtils.generateToken({id: user.id, username: user.username})
+        res.json({token}) 
+    }
+    catch (err) {
+        console.error('Error during login', err)
+        res.status(500).json({message: 'Internal server error'})
     }
 }
