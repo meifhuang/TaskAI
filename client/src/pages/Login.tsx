8dcff1a7308fb64d4024f0069b6d19d16974eb5b
Mei Huang <100555326+meifhuang@users.noreply.github.com>
2023-09-23 14:35:17 -0400

    Style Login page

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -7,12 +7,48 @@ import Container from '@mui/material/Container';
 import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Box';
 import Link from '@mui/material/Link';
-
+import Image from '../assets/login.jpg';
+import Background from '../assets/background.jpg'; 
 
 
 // import ToDoItem from "./ToDoItem"
 // import {Todo} from "../model"
 // import AddIcon from '@mui/icons-material/Add';
+
+const loginStyles = {
+    container: {
+        height: '100vh',
+        display: 'flex', 
+        justifyContent:'center', 
+        alignItems: 'center',
+        backgroundImage: `url(${Background})`,
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+    }, 
+    box: {
+        height: '80%',
+        width: '80%',
+        display: 'flex', 
+        justifyContent:'center', 
+        alignItems: 'center',
+        boxShadow: '0px 0px 25px 10px rgba(20, 0, 0, 0.1)',
+        backgroundColor: 'rgba(0, 0, 0, 0.1)'
+
+    },
+    imageBox: {
+        width: '100%',
+        height: '100%',
+    },
+    loginBox: {
+        width: '80%', 
+        height: '100%',
+        display:'flex', 
+        flexDirection:'column',
+        justifyContent:'center', 
+        alignItems: 'center',
+        backgroundColor: 'white'
+    }
+}
 
 
 const Login: React.FC = () => {
@@ -58,19 +94,21 @@ const Login: React.FC = () => {
     }
 
   return (
-    <Container maxWidth='md' sx={{height: '100vh', display:'flex', justifyContent:'center', alignItems: 'center'}}>  
-    <Box
-      component="form"
-      noValidate
-      autoComplete="off"
-      onSubmit={handleSubmit}
-      sx= {{ height: '20em', width: '29em' , border: '1px solid black', display:'flex', flexDirection:'column',justifyContent:'center', alignItems: 'center'}}
-      px={10}
-      py={5}
-     
-    >
-        <Typography variant='h3' m={2}> Login </Typography>
-        <TextField
+    <Box sx={loginStyles.container} p={0} m={0}>
+       <Box sx={loginStyles.box}> 
+            <Box sx={loginStyles.imageBox}> 
+            <img src={Image} width='100%' height='100%'/> 
+            </Box>
+            <Box
+            component="form"
+            noValidate
+            autoComplete="off"
+            onSubmit={handleSubmit}
+            sx= {loginStyles.loginBox}
+            px={10}
+            >
+            <Typography variant='h3' m={2}> Login </Typography>
+            <TextField
             variant="outlined"
             type="text" 
             name="username"
@@ -82,7 +120,7 @@ const Login: React.FC = () => {
             fullWidth
             sx={{py: 1}}
             />
-        <TextField
+            <TextField
             variant="outlined"
             type="password" 
             name="password"
@@ -92,16 +130,16 @@ const Login: React.FC = () => {
             required
             label="password"
             fullWidth 
-           
-            />
-        <Box sx={{display: 'flex'}}> 
-            <Typography align='center' color='primary' m={2}> Don't have an account? <Link href='/register'> Sign up here </Link> </Typography> 
-           
-        </Box>
-        <Button variant="contained" type="submit" color="success"> Submit </Button>
-    </Box>
 
-    </Container>
+            />
+            <Box sx={{display: 'flex'}}> 
+            <Typography align='center' color='primary' m={2}> Don't have an account? 
+            <Link href='/register'> Sign up here </Link> </Typography> 
+            </Box>
+            <Button variant="contained" type="submit" color="success"> Submit </Button>
+            </Box>
+        </Box>
+    </Box>
   )
 }
 
