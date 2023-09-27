591fb1ac990a582887ec3aa92bcffb2ba2c81404
Mei Huang <100555326+meifhuang@users.noreply.github.com>
2023-09-27 12:28:36 -0400

    Remove previous frontend folder

--- client/src/components/VirtualAssistant.tsx
+++ client/src/components/VirtualAssistant.tsx
@@ -1,8 +1,12 @@
 import React, {useState} from 'react';
+import 'regenerator-runtime'
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 import MicIcon from '@mui/icons-material/Mic';
 import MicOffIcon from '@mui/icons-material/MicOff';
+import Button from '@mui/material/Button'; 
 import Box from '@mui/material/Box';
+import { Typography } from '@mui/material';
+
 
 const virtualStyles = {
   chatbox: {
@@ -12,14 +16,30 @@ const virtualStyles = {
     flexDirection: 'column',
     alignItems: 'center',
     backgroundColor: 'white',
-    borderRadius: '6px'
+    borderRadius: '6px',
+    padding: '1.5em'
   },
-  command_box: {
-    border: '1px solid black'
+  command_container: {
+  },
+  command_options: {
+      border: '1px solid black',
+      borderRadius: '10px',
+      padding: '1em 2em',
+      margin: '1em'
+  },
+  microphone_button: {
+    backgroundColor: 'white',
+    borderRadius: '50%',
+    border: '1px solid gray', 
+    padding: '1.3em',
+    margin: '1em'
   }
 }
 
 const VirtualAssistant: React.FC = () => {
+
+  const command_list = ["Add ______ to my list", "Mark ______ as done", "Edit task ______ to ______", "Remove ______ from my list"] 
+
 
   const commands = [
     {
@@ -65,12 +85,18 @@ const VirtualAssistant: React.FC = () => {
 
   return (
     <Box sx={virtualStyles.chatbox}>
-      <h1> Virtual Assistant </h1>
-      <h3> Hey there, what can I do for you today? </h3>
-      <p> Here are some suggestions</p> 
-      <Box sx={virtualStyles.command_box}> 
-          hello
+      <Typography variant='h2'> Virtual Assistant </Typography>
+      <Typography variant='h6'> Hey there, what can I do for you today? </Typography>
+      <Typography> Here are some suggestions</Typography> 
+      <Box sx={virtualStyles.command_container}> 
+          {command_list.map((command)=> (
+            <Box sx={virtualStyles.command_options}> 
+              <Typography> {command} </Typography>
+             </Box>
+          ))}
       </Box>
+      <Button sx={virtualStyles.microphone_button} onClick={handleMic}> {listening ? <MicIcon/> : <MicOffIcon/>} </Button>
+      {transcript}
     </Box>
   )
 }
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import 'regenerator-runtime/runtime';
-
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
